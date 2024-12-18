31c70080899da12bb9c6f13350922896cf0eb62f
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2024-12-18 16:06:20 +0100

    correzione errore posizonamento directory

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 // stampiamo i valori di i da 1 a 100
-const i = 1;
 
 for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    
+    // se multiplto di 3 mostra Fizz
     if (i % 3 === 0) {
-    console.log("il valore di i è: ", i);
+    console.log("Fizz");
+    }   
+     // se multiplto di 5 mostra Buzz
+    if  (i % 5 === 0) {
+    console.log("Buzz");
+    }
+     // se multiplto sia di 3 che di 5 mostra FizzBuzz
+    if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
     }
 }
