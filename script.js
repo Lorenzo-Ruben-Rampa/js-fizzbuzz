d983314d842c2ce240bd6c46c898a8a9d9a01ec8
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2024-12-18 16:04:39 +0100

    modifiche js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // stampiamo i valori di i da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
+    console.log("il valore di i è: ", i);
+    
     // se multiplto di 3 mostra Fizz
     if (i % 3 === 0) {
     console.log("Fizz", i);
