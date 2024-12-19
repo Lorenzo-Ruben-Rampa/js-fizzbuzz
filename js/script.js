9ae2e171d3b25ca08c9537441776234c1f02c367
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2024-12-19 14:28:33 +0100

    correzione errore logico

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 // stampiamo i valori di i da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    
-    // se multiplto di 3 mostra Fizz
-    if (i % 3 === 0) {
+   
+    // se multiplto sia di 3 che di 5 mostra FizzBuzz
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("FizzBuzz");
+    }
+     // se multiplto di 3 mostra Fizz
+    else if (i % 3 === 0) {
     console.log("Fizz");
     }   
      // se multiplto di 5 mostra Buzz
-    if  (i % 5 === 0) {
+    else if  (i % 5 === 0) {
     console.log("Buzz");
     }
-     // se multiplto sia di 3 che di 5 mostra FizzBuzz
-    if (i % 5 === 0 && i % 3 === 0) {
-    console.log("FizzBuzz");
+    else {
+    console.log(i);
     }
 }
